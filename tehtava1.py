78cd283a7ba6c38a4666e882927a959d3c9f5871
ChiefSeve <65978635+ChiefSeve@users.noreply.github.com>
2023-10-31 09:25:03 +0200

    Update tehtava1.py

--- tehtava1.py
+++ tehtava1.py
@@ -17,3 +17,13 @@ x = 1.6
 y = 2.3
 z = np.hypot(x, y)
 print(z, 'Hypotenuusa')
+
+
+# Tässä on myös korjattu lentokonetehtävä
+
+'''
+s = vt = 950kmh * ((1000m/km)/(3600s/h)) * 45 # 45s
+=11875  # m
+x = s tan(35) = 11875m  * tan(35)
+= 8314 = 8300
+'''
